a2e3d2189a58d9a90e98fa600dcb1bead45eda41
radovanbotik <radovanbotik@gmail.com>
2022-09-09 23:09:24 +0200

    ""

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const listItems = [...document.querySelectorAll(".timeline-list__item")];
+const timelineList = document.querySelector(".timeline-list");
+
+const loadNewCards = () => {
+  for (let i = 0; i < 10; i++) {
+    const card = document.createElement("li");
+    card.classList.add("timeline-list__item");
+    card.classList.add("glass");
+
+    card.innerHTML = `
+        <h1>96<span>kg</span></h1>
+          <h4>April 1st</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias,
+            cum?
+          </p>
+        `;
+    timelineList.appendChild(card);
+    observer.observe(card);
+  }
+};
 
 const observer = new IntersectionObserver(
   entries => {
     entries.forEach(entry => {
-      console.log(entry.target);
       entry.target.classList.toggle("visible", entry.isIntersecting);
       if (entry.isIntersecting) observer.unobserve(entry.target);
     });
@@ -12,6 +31,17 @@ const observer = new IntersectionObserver(
     threshold: 1,
   }
 );
+
+const lastCardObserver = new IntersectionObserver(entries => {
+  const lastCard = entries[0];
+  if (!lastCard.isIntersecting) return;
+  loadNewCards();
+  observer.unobserve(lastCard);
+  observer.observe(listItems[listItems.length - 1]);
+}, {});
+
+lastCardObserver.observe(listItems[listItems.length - 1]);
+
 listItems.forEach(listItem => {
   observer.observe(listItem);
 });
